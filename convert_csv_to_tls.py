be8155e418fa1cb4cdf619ce6366c1e7dcc4aae1
Shuiling <132723284+shulng@users.noreply.github.com>
2024-06-16 17:15:14 -0700

    更新 convert_csv_to_tls.py

--- convert_csv_to_tls.py
+++ convert_csv_to_tls.py
@@ -1,21 +1,19 @@
 import csv
 
-def convert_csv_to_tls():
-    csv_filename = 'result.csv'
-    output_filename = 'TLS.txt'
-    
+def convert_csv_to_tls(csv_filename, output_filename):
     transformed_data = []
-    with open(csv_filename, 'r', newline='', encoding='utf-8') as infile:
+    with open(csv_filename, 'r', encoding='utf-8') as infile:
         reader = csv.reader(infile)
-        next(reader)
-        for row in reader:
-            original_ip = row[0]
-            formatted_ip = f"{original_ip}:443#测试节点请勿测速"
+        next(reader)  # Skip header
+        for original_ip in reader:
+            formatted_ip = f"{original_ip[0]}:443#测试节点请勿测速"
             transformed_data.append(formatted_ip)
 
-    with open(output_filename, 'w', newline='', encoding='utf-8') as outfile:
+    with open(output_filename, 'w', encoding='utf-8') as outfile:
         for data in transformed_data:
             outfile.write(data + '\n')
 
 if __name__ == "__main__":
-    convert_csv_to_tls()
+    csv_filename = 'result.csv'
+    output_filename = 'TLS.txt'
+    convert_csv_to_tls(csv_filename, output_filename)
