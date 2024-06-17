0bd97a6180f41a868ded2af2f70c448569eb0b3f
Shuiling <1668589925@qq.com>
2024-06-17 01:54:33 -0700

    a

--- convert_csv_to_tls.py
+++ convert_csv_to_tls.py
@@ -4,7 +4,7 @@ def convert_csv_to_tls(csv_filename, output_filename):
     transformed_data = []
     with open(csv_filename, 'r', encoding='utf-8') as infile, open(output_filename, 'w', encoding='utf-8') as outfile:
         reader = csv.reader(infile)
-        next(reader)  # 跳过标题行
+        next(reader)
         for row in reader:
             formatted_ip = f"{row[0]}:443#自动优选IP"
             outfile.write(formatted_ip + '\n')
