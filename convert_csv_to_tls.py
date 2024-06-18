14713d71653b32b4294f857c52840b035ae8dfcb
Shuiling <1668589925@qq.com>
2024-06-18 07:19:58 -0700

    feat(convert_csv_to_tls.py): 增加TLS和notls参数功能

--- convert_csv_to_tls.py
+++ convert_csv_to_tls.py
@@ -1,15 +1,21 @@
 import csv
+import sys
 
-def convert_csv_to_tls(csv_filename, output_filename):
+def convert_csv_to_tls(csv_filename, TLS, notls=False):
     transformed_data = []
+    output_filename = notls if notls else TLS
     with open(csv_filename, 'r', encoding='utf-8') as infile, open(output_filename, 'w', encoding='utf-8') as outfile:
         reader = csv.reader(infile)
         next(reader)
         for row in reader:
-            formatted_ip = f"{row[0]}:443#自动优选IP"
+            if notls:
+                formatted_ip = f"{row[0]}:80#自动优选IP"
+            else:
+                formatted_ip = f"{row[0]}:443#自动优选IP"
             outfile.write(formatted_ip + '\n')
 
 if __name__ == "__main__":
     csv_filename = 'result.csv'
-    output_filename = 'TLS.txt'
-    convert_csv_to_tls(csv_filename, output_filename)
+    TLS = 'TLS.txt'
+    notls = False if len(sys.argv) == 1 else 'notls.txt'
+    convert_csv_to_tls(csv_filename, TLS, notls)
