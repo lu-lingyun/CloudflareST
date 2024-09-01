b960fa22d01ceb9dc3abcfd1b1d9aade3bbeb2c7
Shuiling <132723284+shulng@users.noreply.github.com>
2024-09-01 10:51:37 +0800

    更新 convert_csv_to_tls.py

--- convert_csv_to_tls.py
+++ convert_csv_to_tls.py
@@ -1,6 +1,6 @@
 import csv
 import requests
-import sys  # 添加sys模块的导入
+import sys
 
 def get_location(ip):
     try:
@@ -13,7 +13,7 @@ def get_location(ip):
 def convert_csv_to_tls(csv_filename, output_filename, notls=False):
     with open(csv_filename, 'r', encoding='utf-8') as infile, open(output_filename, 'w', encoding='utf-8') as outfile:
         reader = csv.reader(infile)
-        next(reader)  # Skip header row
+        next(reader)
         for row in reader:
             ip = row[0]
             countryCode = get_location(ip)
@@ -21,12 +21,10 @@ def convert_csv_to_tls(csv_filename, output_filename, notls=False):
             port = ":443" if not notls else ":80"
             formatted_ip = f"{ip}{port}#{variable}-自动优选"
             if notls:
-                formatted_ip += "-自动优选快速-[使用本节点时请勿访问/发布色情涉政等非法内容]"
+                formatted_ip += "-快速-[使用本节点时请勿访问/发布色情涉政等非法内容]"
             outfile.write(formatted_ip + '\n')
 
 if __name__ == "__main__":
     csv_filename = 'result.csv'
-    # Determine the output filename based on the notls flag
-    # If no argument is provided, default to 'TLS.txt'
     output_filename = 'notls.txt' if len(sys.argv) > 1 and sys.argv[1] == 'notls' else 'TLS.txt'
     convert_csv_to_tls(csv_filename, output_filename, notls=(output_filename == 'notls.txt'))
