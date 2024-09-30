86f052fee668024963d06e7618641f8927f41f45
Shuiling <132723284+shulng@users.noreply.github.com>
2024-10-01 02:46:22 +0800

    更新 a.py

--- a.py
+++ a.py
@@ -4,7 +4,7 @@ with open('ip.txt', 'r') as file, open('open_ips.txt', 'w') as output:
     for line in file:
         ip = line.split('/')[0].strip()
         sock = socket.socket(socket.AF_INET6 if ':' in ip else socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
+        sock.settimeout(5)
         if sock.connect_ex((ip, 443)) == 0:
             output.write(ip + '\n')
         sock.close()
