179d6104dcf8b9fd6e236283947fa7eeef6b7498
Shuiling <132723284+shulng@users.noreply.github.com>
2024-10-01 02:12:16 +0800

    Add files via upload

---
+++ a.py
@@ -0,0 +1,10 @@
+import socket
+
+with open('ip.txt', 'r') as file, open('open_ips.txt', 'w') as output:
+    for line in file:
+        ip = line.split('/')[0].strip()
+        sock = socket.socket(socket.AF_INET6 if ':' in ip else socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        if sock.connect_ex((ip, 443)) == 0:
+            output.write(ip + '\n')
+        sock.close()
