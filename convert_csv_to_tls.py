6dbb3491e7ee649a2f94fb4a0c0738587f331c7e
Shuiling <132723284+shulng@users.noreply.github.com>
2024-10-03 01:55:28 +0800

    更新 convert_csv_to_tls.py

--- convert_csv_to_tls.py
+++ convert_csv_to_tls.py
@@ -2,26 +2,13 @@ import csv
 import requests
 import sys
 
-def get_location(ip):
-    try:
-        response = requests.get(f'http://ip-api.com/json/{ip}')
-        data = response.json()
-        return data['countryCode'] if data['status'] == 'success' else None
-    except requests.RequestException:
-        return None
-
 def convert_csv_to_tls(csv_filename, output_filename, notls=False):
     with open(csv_filename, 'r', encoding='utf-8') as infile, open(output_filename, 'w', encoding='utf-8') as outfile:
         reader = csv.reader(infile)
         next(reader)
         for row in reader:
             ip = row[0]
-            countryCode = get_location(ip)
-            variable = f"[{countryCode}]" if countryCode else "[位置获取失败]"
-            port = ":443" if not notls else ":80"
-            formatted_ip = f"{ip}{port}#{variable}-自动优选"
-            if notls:
-                formatted_ip += "-快速-[NOTLS]"
+            formatted_ip = f"{ip}"
             outfile.write(formatted_ip + '\n')
 
 if __name__ == "__main__":
