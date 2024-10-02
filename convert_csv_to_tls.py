f8a34a99896f5610c8fa4f136b86a7f416818d88
Shuiling <132723284+shulng@users.noreply.github.com>
2024-10-03 02:01:42 +0800

    更新 convert_csv_to_tls.py

--- convert_csv_to_tls.py
+++ convert_csv_to_tls.py
@@ -1,17 +1,15 @@
 import csv
-import requests
 import sys
 
-def convert_csv_to_tls(csv_filename, output_filename, notls=False):
+def convert_csv_to_tls(csv_filename, output_filename):
     with open(csv_filename, 'r', encoding='utf-8') as infile, open(output_filename, 'w', encoding='utf-8') as outfile:
         reader = csv.reader(infile)
-        next(reader)
+        next(reader)  # 跳过标题行（如果有的话）
         for row in reader:
             ip = row[0]
-            formatted_ip = f"{ip}"
-            outfile.write(formatted_ip + '\n')
+            outfile.write(ip + '\n')
 
 if __name__ == "__main__":
     csv_filename = 'result.csv'
     output_filename = 'notls.txt' if len(sys.argv) > 1 and sys.argv[1] == 'notls' else 'TLS.txt'
-    convert_csv_to_tls(csv_filename, output_filename, notls=(output_filename == 'notls.txt'))
+    convert_csv_to_tls(csv_filename, output_filename)
