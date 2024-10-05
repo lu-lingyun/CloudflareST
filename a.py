f2e14009060c2baea170dc97862c17ac4ff2307e
Shuiling <1668589925@qq.com>
2024-10-05 00:49:43 -0700

    a

--- a.py
+++ a.py
@@ -1,6 +1,6 @@
 import socket
 
-with open('ip.txt', 'r') as file, open('open_ips.txt', 'w') as output:
+with open('ip.txt') as file, open('open_ips.txt', 'w') as output:
     for line in file:
         ip = line.split('/')[0].strip()
         sock = socket.socket(socket.AF_INET6 if ':' in ip else socket.AF_INET, socket.SOCK_STREAM)
