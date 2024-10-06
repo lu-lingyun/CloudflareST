e190cd18a7d409f1b206d9d28a4bcf31052cff7a
Shuiling <1668589925@qq.com>
2024-10-06 01:59:18 -0700

    a

--- convert_csv_to_tls.py
+++ convert_csv_to_tls.py
@@ -4,8 +4,8 @@ import sys
 def convert_csv_to_tls(csv_filename, output_filename):
     # 使用pandas读取CSV文件
     df = pd.read_csv(csv_filename, encoding='utf-8')
-    # 选取第一列，并且只取前10行
-    ips = df.iloc[:, 0].head(10)
+    # 选取第一列的所有行
+    ips = df.iloc[:, 0]
     # 将IP地址写入文件，每个IP地址一行
     ips.to_csv(output_filename, index=False, header=False)
 
