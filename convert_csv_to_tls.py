d7a0ae5e96d552f1a6615f80bb9188ac973f709c
灵 <132723284+shulng@users.noreply.github.com>
2025-02-28 21:22:10 +0800

    更新 convert_csv_to_tls.py

--- convert_csv_to_tls.py
+++ convert_csv_to_tls.py
@@ -2,14 +2,8 @@ import pandas as pd
 import sys
 
 def convert_csv_to_tls(csv_filename, output_filename):
-    # 使用pandas读取CSV文件
-    df = pd.read_csv(csv_filename, encoding='utf-8')
-    # 选取第一列的所有行
-    ips = df.iloc[:, 0]
-    # 将IP地址写入文件，每个IP地址一行
-    ips.to_csv(output_filename, index=False, header=False)
+    pd.read_csv(csv_filename, encoding='utf-8').iloc[:, 0].to_csv(output_filename, index=False, header=False)
 
-# 假设csv_filename为 'result.csv'
 csv_filename = 'result.csv'
 output_filename = 'notls.txt' if len(sys.argv) > 1 and sys.argv[1] == 'notls' else 'TLS.txt'
 convert_csv_to_tls(csv_filename, output_filename)
